cb2a11c79630e01b805fd76743f3620a452239b1
virnaafrlnty <virnaafrilianty23@gmail.com>
2024-06-21 23:18:24 +0800

    Membuat login & Register

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,39 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [DashboardController::class, 'index'])->middleware('auth');
+
+
+Route::get('/admin', function () {
+    return view('frontend.admin.admin');
 });
 
-Route::get('/stock', function () {
-    return view('stock.index');
+Route::get('/admin/crud-tambah', function () {
+    return view('frontend.admin.create');
 });
 
-Route::get('/users', function () {
-    return view('users.index');
+Route::get('/admin/crud-edit', function () {
+    return view('frontend.admin.edit');
 });
+
+Route::get('/manajemen-staff', function () {
+    return view('frontend.admin.manajemen-staff');
+});
+Route::get('stockout', function () {
+    return view('frontend.admin.stokout');
+});
+Route::get('stockin', function () {
+    return view('frontend.admin.stockin');
+});
+
+
+Route::get('/register', [AuthController::class, 'registerForm'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
