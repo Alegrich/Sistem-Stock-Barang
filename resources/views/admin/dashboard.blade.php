68a6c3f83064bbee7bbf2054433e7c862b40ccc5
Eza06 <ezamohammad06@gmail.com>
2024-06-27 14:59:56 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -57,7 +57,7 @@
                     </div>
                 </div>
             </div>
-        </div> 
+        </div>
         {{-- card end --}}
 
         {{-- table --}}
@@ -75,7 +75,7 @@
                 class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h4>Tabel Data Barang</h4>
                 <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                    
+
                     <button type="button" class="btn btn-primary  bi bi-file-earmark-arrow-down position-relative">
                         Unduh
                         data</button>
@@ -94,85 +94,11 @@
                     </tr>
                 </thead>
                 <tbody>
-                    
+
                 </tbody>
             </table>
         </div>
         {{-- table end --}}
-    </div>
-
-@endsection
-@extends('layouts.parent')
-@section('title', 'Users')
-@section('content')
-    <div class="container-xxl flex-grow-1 container-p-y">
-        <div class="row g-4 mb-4">
-            <div class="col-sm-6 col-xl-3">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex align-items-start justify-content-between">
-                            <div class="content-left">
-                                <span>Staff</span>
-                                <div class="d-flex align-items-end mt-2">
-                                    <h4 class="mb-0 me-2">2s1,459</h4>
-                                </div>
-                            </div>
-                            <div class="avatar">
-                                <span class="avatar-initial rounded bg-label-primary">
-                                    <i class="bx bx-user bx-sm"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex align-items-start justify-content-between">
-                            <div class="content-left">
-                                <span>Supplier</span>
-                                <div class="d-flex align-items-end mt-2">
-                                    <h4 class="mb-0 me-2">2s1,459</h4>
-                                </div>
-                            </div>
-                            <div class="avatar">
-                                <span class="avatar-initial rounded bg-label-primary">
-                                    <i class="bx bx-user bx-sm"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-6 col-xl-3">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex align-items-start justify-content-between">
-                            <div class="content-left">
-                                <span>Jumlah Items</span>
-                                <div class="d-flex align-items-end mt-2">
-                                    <h4 class="mb-0 me-2">2s1,459</h4>
-                                </div>
-                            </div>
-                            <div class="avatar">
-                                <span class="avatar-initial rounded bg-label-primary">
-                                    <i class="bx bx-user bx-sm"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Users List Table -->
-
-        
-        {{-- Statistik --}}
-        <div class="card">
-            <canvas id="myChart"></canvas>
-        </div>
-        
     </div>
 
 @endsection
